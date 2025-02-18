2e78ac841e19723f17169cadf60586d621048668
Jesús Amador <jamador@banhcafe.hn>
2025-02-18 16:39:07 -0600

    primer commit

---
+++ ManejoPresupuesto/Models/TipoCuenta.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ManejoPresupuesto.Validaciones;
+
+namespace ManejoPresupuesto.Models
+{
+    public class TipoCuenta : IValidatableObject
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="El campo {0} es requrido")]
+        /* [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage ="La longitud del campo {0} debe estar entre {2} y {1}")]*/
+        //[PrimeraLetraMayuscula]
+        public string Nombre { get; set; }
+
+        public int UsuarioId { get; set; }
+
+        public int Orden { get; set; }
+
+        public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+        {
+            if (Nombre != null && Nombre.Length > 0)
+            {
+                var primeraLetra = Nombre[0].ToString();
+                if(primeraLetra != primeraLetra.ToUpper())
+                {
+                    yield return new ValidationResult("La primera letra debe ser mayuscula");
+
+
+                }
+            }
+        }
+    }
+}
